d440cab37be66d812ff991468f015f5352673028
Avinash Yadav <avinashy1993@gmail.com>
2025-12-03 20:13:12 +0530

    Created the Animation for the Builder and Warrior

--- Assets/Project/Scripts/Units/HumanoidUnit.cs
+++ Assets/Project/Scripts/Units/HumanoidUnit.cs
@@ -1,4 +1,20 @@
+using UnityEngine;
 public class HumanoidUnit : Unit
 {
+    protected Vector2 velocity;
+    protected Vector3 lastPosition;
+
+
+    protected void Update()
+    {
+        velocity = new Vector2(
+            (transform.position.x - lastPosition.x) / Time.deltaTime,
+            (transform.position.z - lastPosition.z) / Time.deltaTime
+        );
+
+        lastPosition = transform.position;
+        isMoving = velocity.magnitude > 0.1f;
+    }
+
 
 }
--- Assets/Project/Scripts/Units/Unit.cs
+++ Assets/Project/Scripts/Units/Unit.cs
@@ -1,6 +1,7 @@
+using System;
 using UnityEngine;
 
 public abstract class Unit : MonoBehaviour
 {
-
+    [SerializeField] protected bool isMoving;
 }
