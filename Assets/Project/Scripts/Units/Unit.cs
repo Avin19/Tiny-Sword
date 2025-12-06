19e0bc611fb6cdc835a0b04bf2a2343227e7f2fc
Avinash Yadav <avinashy1993@gmail.com>
2025-12-06 18:43:43 +0530

    Added the Functionality of Fliping

--- Assets/Project/Scripts/Units/Unit.cs
+++ Assets/Project/Scripts/Units/Unit.cs
@@ -7,15 +7,20 @@ public abstract class Unit : MonoBehaviour
     protected Animator animator;
     protected AIPawn _aiPawn;
 
+    protected SpriteRenderer _spriteRenderer;
+
 
     protected void Awake()
     {
         animator = TryGetComponent<Animator>(out var anim) ? anim : null;
         _aiPawn = TryGetComponent<AIPawn>(out var aiPawn) ? aiPawn : null;
+        _spriteRenderer = TryGetComponent<SpriteRenderer>(out var spriteRenderer) ? spriteRenderer : null;
     }
 
     public void MoveToPosition(Vector3 position)
     {
+        var direction = (position - transform.position).normalized;
+        _spriteRenderer.flipX = direction.x < 0;
         if (_aiPawn != null)
         {
             _aiPawn.SetDestination(position);
